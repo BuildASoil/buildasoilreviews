1dbc2b367f5c6511ed3660cf3cc86f06914aed88
Jeremy <jeremy@buildasoil.com>
2026-05-19 12:02:51 -0600

    Fix sql.js WASM file inclusion on Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Tell Vercel/Next to bundle the WASM file alongside server functions
+  outputFileTracingIncludes: {
+    '/**/*': [
+      './node_modules/sql.js/dist/sql-wasm.wasm',
+      './data/reviews.db',
+    ],
+  },
+  // Keep sql.js as a server-external package so it's not bundled by Webpack
   serverExternalPackages: ['sql.js'],
   webpack: (config, { isServer }) => {
     if (isServer) {
